48f53e25036b375856965bbeb73706c2cc261930
Aditi Jha <54998338+aditijhaa9901@users.noreply.github.com>
2020-07-17 09:08:29 +0530

    Create Day3,qstn3.js

---
+++ Day3,qstn3.js
@@ -0,0 +1,12 @@
+grade= prompt("enter your grade", "you didnt enter your marks correctly")
+if (grade >= 90) {
+  console.log("A");
+} else if (grade >= 80) {
+  console.log("B");
+} else if (grade >= 70) {
+  console.log("C");
+} else if (grade >= 60) {
+  console.log("D");
+} else {
+  console.log("F");
+}
